34790fcede3860354e0c54016e6c5da34cbf80fb
Dolores Fuertes <dolores@fuertesdebarriga.com>
2017-11-23 15:41:44 +0100

    Versión con Dockerfile

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var path = require('path');
+
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', function(req, res) {
+  res.send('Hemos recibido su petición');
+});
+
+app.get('/clientes/:idcliente', function (req, res) {
+  res.send('Aquí tiene al cliente número ' + req.params.idcliente);
+})
