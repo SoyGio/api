e1560ddaf1e3c6731bf46fcab13439ab084d874d
Edgar Ballesteros <eballesterosf@hotmail.com>
2018-07-20 06:35:31 +0200

    Version Inicial

--- server.js
+++ server.js
@@ -7,15 +7,3 @@ var path = require('path');
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.post('/', function(req, res) {
-  res.send('Hemos recibido su petición cambiada');
-});
-
-app.get('/clientes/:idcliente', function (req, res) {
-  res.send('Aquí tiene al cliente número ' + req.params.idcliente);
-})
