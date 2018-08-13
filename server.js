d79ea6d826446c6a05ca9ec79879a08e8af9884a
Giovanni Nicolás Marín <giovanni.nicolas.marin@bbva.com>
2018-08-13 17:30:02 -0500

    Esqueleto api node

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ var path = require('path');
 
 app.listen(port);
 
+app.get("/",function(req,res){
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post("/", function(req, res){
+	res.send("Hemos recibido tu petición");
+});
+
+app.put("/",function(req,res){
+	res.send("Se han actualizado los datos");
+});
+
+app.delete("/",function(req,res){
+	res.send("Se ha eliminado el dato");
+});
+
 console.log('todo list RESTful API server started on: ' + port);
