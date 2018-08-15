25ce864325b43f8ab448d1f6cef0cc16bb1e90e8
Giovanni Nicolás Marín <giovanni.nicolas.marin@bbva.com>
2018-08-15 15:47:32 -0500

    add docker

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ var path = require('path');
 
 app.listen(port);
 
-app.get("/",function(req,res){
+app.get("/", function(req,res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get("/clientes/:id", function(req, res){
+	res.send("Se recibió el cliente número: " + req.params.id);
+})
 
 app.post("/", function(req, res){
 	res.send("Hemos recibido tu petición");
@@ -21,5 +25,6 @@ app.put("/",function(req,res){
 app.delete("/",function(req,res){
 	res.send("Se ha eliminado el dato");
 });
+
 
 console.log('todo list RESTful API server started on: ' + port);
