76016d881e839c3b2f76cd7b5a3bfb2c44c8a374
Edgar Ballesteros <edgar.ballesteros.contractor@bbva.com>
2018-08-15 23:18:01 +0200

    Funciona

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ var path = require('path');
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get("/clientes/:idcliente", function(req, res) {
+  res.send("Aquí tiene al cliente número: "+req.params.idcliente);
+})
+
+app.post("/", function(req, res) {
+  res.send("Hemos recibido su petición");
+})
+
+app.put("/", function(req, res) {
+  res.send("Hemos recibido su petición de modificación");
+})
+
+app.delete("/", function(req, res) {
+  res.send("Hemos recibido su petición de borrado");
+})
