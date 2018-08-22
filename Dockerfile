ca04305b6960eb1a39f92909e9375ebde37f02f7
Giovanni Nicolás Marín <giovanni.nicolas.marin@bbva.com>
2018-08-22 16:23:14 -0500

    update changes

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,6 @@
 FROM node:latest
 
 #Directorio de la app
-<<<<<<< HEAD
-WORKDIR /app
-
-#Copiado de archivos
-ADD . /app
-
-#Dependencias
-RUN npm install
-=======
 WORKDIR /api
 
 #Copiado de archivos
@@ -18,14 +9,9 @@ ADD . /api
 
 #Dependencias
 #RUN npm install
->>>>>>> 0ce4a057d0fe937774168d40d80c555b10dc3b59
 
 #Puerto que expongo
 EXPOSE 3000
 
 #Comando
-<<<<<<< HEAD
 CMD ["npm", "start"]
-=======
-CMD ["npm", "start"]
->>>>>>> 0ce4a057d0fe937774168d40d80c555b10dc3b59
--- server.js
+++ server.js
@@ -2,49 +2,67 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var requestjson = require('request-json');
+var bodyParser = require('body-parser');
+
+var urlMovs = "https://api.mlab.com/api/1/databases/bdbanca2/collections/movimientos?apiKey=AIle8Dm6ZzZztpvwolRU4AX2PH9-irFh";
+var clienteMLab = requestjson.createClient(urlMovs);
 var path = require('path');
 
 app.listen(port);
+app.use(bodyParser.json());
 
 app.get("/", function(req,res){
-	res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get("/movimientos", function(req, res){
+  clienteMLab.get('', function(err, resM, body) {
+    if (err){
+      console.log(body);
+    }else{
+      res.send(body);
+    }
+  });
+});
+
+app.post("/movimientos", function(req, res){
+  clienteMLab.headers['Content-Type'] = 'application/json';
+  clienteMLab.post('', req.body, function(err, resM, body) {
+    if (err){
+      console.log(body);
+    }else{
+      res.send(body);
+    }
+  });
+});
+
+app.get("/post", function(req, res){
+  clienteMLab.get('', function(err, resM, body) {
+    if (err){
+      console.log(body);
+    }else{
+      res.send(body);
+    }
+  });
+});
+
+
 app.get("/clientes/:id", function(req, res){
-	res.send("Se recibió el cliente número: " + req.params.id);
+  res.send("Se recibió el cliente número: " + req.params.id);
 })
 
 app.post("/", function(req, res){
-	res.send("Hemos recibido tu petición POST");
+  res.send("Hemos recibido tu petición POST");
 });
 
 app.put("/",function(req,res){
-	res.send("Se han actualizado los datos");
+  res.send("Se han actualizado los datos");
 });
 
 app.delete("/",function(req,res){
-	res.send("Se ha eliminado el dato");
+  res.send("Se ha eliminado el dato");
 });
 
 
 console.log('todo list RESTful API server started on: ' + port);
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-})
-
-app.get("/clientes/:idcliente", function(req, res) {
-  res.send("Aquí tiene al cliente número: "+req.params.idcliente);
-})
-
-app.post("/", function(req, res) {
-  res.send("Hemos recibido su petición actualizada");
-})
-
-app.put("/", function(req, res) {
-  res.send("Hemos recibido su petición de modificación");
-})
-
-app.delete("/", function(req, res) {
-  res.send("Hemos recibido su petición de borrado");
-})
