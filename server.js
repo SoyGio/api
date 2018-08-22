0ce4a057d0fe937774168d40d80c555b10dc3b59
Giovanni Nicolás Marín <giovanni.nicolas.marin@bbva.com>
2018-08-22 15:54:44 -0500

    add all changes

--- server.js
+++ server.js
@@ -2,13 +2,51 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var requestjson = require('request-json');
+var bodyParser = require('body-parser');
+
+var urlMovs = "https://api.mlab.com/api/1/databases/bdbanca2/collections/movimientos?apiKey=AIle8Dm6ZzZztpvwolRU4AX2PH9-irFh";
+var clienteMLab = requestjson.createClient(urlMovs);
 var path = require('path');
 
 app.listen(port);
+app.use(bodyParser.json());
 
 app.get("/", function(req,res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get("/movimientos", function(req, res){
+	clienteMLab.get('', function(err, resM, body) {
+		if (err){
+			console.log(body);
+		}else{
+			res.send(body);
+		}
+	});
+});
+
+app.post("/movimientos", function(req, res){
+	clienteMLab.headers['Content-Type'] = 'application/json';
+	clienteMLab.post('', req.body, function(err, resM, body) {
+		if (err){
+			console.log(body);
+		}else{
+			res.send(body);
+		}
+	});
+});
+
+app.get("/post", function(req, res){
+	clienteMLab.get('', function(err, resM, body) {
+		if (err){
+			console.log(body);
+		}else{
+			res.send(body);
+		}
+	});
+});
+
 
 app.get("/clientes/:id", function(req, res){
 	res.send("Se recibió el cliente número: " + req.params.id);
